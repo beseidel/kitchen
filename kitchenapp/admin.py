fda001e3619e82ac8948303c1d6e968e8b5e0e06
KevinLin101996 <33579906+KevinLin101996@users.noreply.github.com>
2019-12-01 12:42:21 -0500

    fix form error

--- kitchenapp/admin.py
+++ kitchenapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import User, Kitchen, Menu, WorkingDay
+from .models import User, Kitchen, Menu, WorkingDay, KitchenImage
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Kitchen)
+admin.site.register(KitchenImage)
 admin.site.register(Menu)
 admin.site.register(WorkingDay)
