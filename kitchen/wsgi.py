8972b15d87f24b830c0c7d2f388dadf3fc9fa4f0
KevinLin101996 <33579906+KevinLin101996@users.noreply.github.com>
2019-12-09 15:38:31 -0500

    a

--- kitchen/wsgi.py
+++ kitchen/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchen.settings')
