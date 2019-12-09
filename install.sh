6485c7044d781bb0b1389184d7f5b64c087be236
KevinLin101996 <33579906+KevinLin101996@users.noreply.github.com>
2019-12-09 14:57:53 -0500

    apache

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 
 
-echo -e "To fix Django error, type '1', to install everything type '2' : \c" 
+source ~/kitchen/env/bin/activate
+echo "To fix Django error, type 1 "  
+echo "To install everything type 2" 
+echo "To deploy Django on Apache type 3" 
+echo -e "Option: \c"
 read error
 
 
 if [ $error == 1 ] 
 then
-   echo "alias pip=pip3" >> ~/.bashrc
-   source ~/.bashrc
-   pip install -r install.txt
+   ~/kitchen/env/bin/pip3 install -r install.txt
 elif [ $error == 2 ]
 then 
    sudo apt-get update
    sudo apt-get install python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python3-pip -y
    sudo apt-get install libmysqlclient-dev -y
    pip install -r install.txt
-else
-   echo "Exiting now ..."
-   sleep 2
+else 
+   sudo apt-get update
+   sudo apt-get install -y apache2
+   sudo ufw allow 'Apache Full'
+   sudo systemctl status apache2
+   sudo ufw allow 'OpenSSH'
+   sudo apt-get install curl
+   curl -4 icanhazip.com
 fi
 
