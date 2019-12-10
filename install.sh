547e0b9c5bdd79c93f7f273b4bd28f63c7d3ae89
KevinLin101996 <33579906+KevinLin101996@users.noreply.github.com>
2019-12-10 00:30:00 -0500

    updated shell

--- install.sh
+++ install.sh
@@ -14,14 +14,17 @@ then
    sudo ufw enable
 elif [ $error == 3 ]
 then   
+   HOME=~/kitchen
    sudo apt-get update
    sudo pip3 install virtualenv 
-   virtualenv ~/kitchen/env
-   source ~/kitchen/env/bin/activate
-   ~/kitchen/env/bin/pip3 install -r install.txt
-   ~/kitchen/env/bin/pip3 install django-paypal
-   sudo apt-get update
+   virtualenv $HOME/env
+   source $HOME/env/bin/activate
    
+   python_pip=$HOME/env/bin/pip3
+   $python_pip install -r install.txt
+   $python_pip install django-paypal
+   
+   sudo apt-get update
    sudo apt-get install apache2 libapache2-mod-wsgi-py3 -y
    sudo ufw allow 'Apache Full'
    sudo ufw allow 'OpenSSH'
@@ -34,7 +37,7 @@ then
    then 
       sudo rm /etc/apache2/sites-enabled/000-default.conf
    fi
-   sudo cp ~/kitchen/apache.conf /etc/apache2/sites-enabled/
+   sudo cp $HOME/apache.conf /etc/apache2/sites-enabled/
    sudo service apache2 restart
    
 fi
